2924cacf2289487ccbfe00a3d0a5917b108416c3
renjm <jianmin.ren@jfjun.com>
2019-10-21 15:23:09 +0800

    添加解压缩

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /*
  * @Auther: renjm
  * @Date: 2019-09-24 12:21:00
- * @LastEditTime: 2019-09-25 17:45:04
+ * @LastEditTime: 2019-10-21 15:21:24
  * @Description: 
  */
 const router = require('koa-router')()
@@ -10,7 +10,9 @@ const config = require('../config.json')
 const fs = require('fs')
 const base = require('../util/base')
 const path = require('path');
+const _ = require('lodash')
 const pathToRegexp = require('path-to-regexp')
+var targz = require('targz');
 const getFileList = (startPath) => {
   let result = [];
   function finder(filePath) {
@@ -29,7 +31,11 @@ const getFileList = (startPath) => {
   return result;
 
 }
-router.get('/list', async (ctx, next) => {
+router.get('/', async (ctx, next) => {
+  return ctx.body = "欢迎来到文件服务"
+})
+
+router.post('/list', async (ctx, next) => {
   let files = getFileList('upload');
   files = files.map(file => ({
     fileName: file,
@@ -41,13 +47,31 @@ router.get('/list', async (ctx, next) => {
 router.post('/', async (ctx, next) => {
   const file = ctx.request.files.file;
   const reader = fs.createReadStream(file.path);
-  const ext = file.name.split('.').pop();
-  const originName = file.name.split('.').slice(0, file.name.split('.').length - 1).join('.')
-  if (originName.indexOf('/') > -1) {
-    await base.mkdirSync(path.resolve(__dirname, '../', 'upload', file.name))
+  if (_.endsWith(file.name, 'tar.gz')) {
+    // let dirName = _.get(file.name.split('.'), '0')
+    // let dirPath = path.resolve(__dirname, '../upload', dirName)
+    // if (dirName && fs.existsSync(dirPath) && fs.statSync(dirPath).isDirectory()) {
+    //   return ctx.body = '该gz文件已经上传，请检查';
+    // }
+    targz.decompress({
+      src: file.path,
+      dest: path.resolve(__dirname, '../', 'upload')
+    }, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Done!");
+      }
+    });
+  } else {
+    const ext = file.name.split('.').pop();
+    const originName = file.name.split('.').slice(0, file.name.split('.').length - 1).join('.')
+    if (originName.indexOf('/') > -1) {
+      await base.mkdirSync(path.resolve(__dirname, '../', 'upload', file.name))
+    }
+    const upStream = fs.createWriteStream(`upload/${originName}.${ext}`);
+    reader.pipe(upStream);
   }
-  const upStream = fs.createWriteStream(`upload/${originName}.${ext}`);
-  reader.pipe(upStream);
   return ctx.body = '上传成功';
 })
 
