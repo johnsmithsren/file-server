c629e819e187ca28caf6b72a2744b05af07e1fee
renjm <jianmin.ren@jfjun.com>
2019-10-22 18:52:32 +0800

    代码调整

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /*
  * @Auther: renjm
  * @Date: 2019-09-24 12:21:00
- * @LastEditTime: 2019-10-22 16:21:53
+ * @LastEditTime: 2019-10-22 18:50:32
  * @Description: 
  */
 const router = require('koa-router')()
@@ -12,10 +12,10 @@ const path = require('path');
 const _ = require('lodash')
 const multer = require('koa-multer');
 const fs = require('fs-extra');
-const uploadPath = path.join(__dirname, '../upload');
+const uploadPath = path.join(__dirname, '../hotpatch');
 const uploadTempPath = path.join(uploadPath, 'temp');
 const upload = multer({ dest: uploadTempPath });
-var targz = require('targz');
+const targz = require('targz');
 const getFileList = (startPath) => {
   let result = [];
   function finder(filePath) {
@@ -27,7 +27,7 @@ const getFileList = (startPath) => {
       let fPath = path.join(filePath, val);
       let stats = fs.statSync(fPath);
       if (stats.isDirectory()) { finder(fPath); }
-      if (stats.isFile()) { result.push(fPath.slice(7)); }
+      if (stats.isFile()) { result.push(fPath.slice(9)); }
     });
   }
   finder(startPath);
@@ -39,10 +39,10 @@ router.get('/', async (ctx, next) => {
 })
 
 router.post('/list', async (ctx, next) => {
-  let files = getFileList('upload');
+  let files = getFileList('hotpatch');
   files = files.map(file => ({
     fileName: file,
-    downloadUrl: `${config.domin}/download/${file}`
+    downloadUrl: `${config.domin}/hotpatch/${file}`
   }));
   return ctx.body = { data: files }
 })
@@ -51,14 +51,9 @@ router.post('/', async (ctx, next) => {
   const file = ctx.request.files.file;
   const reader = fs.createReadStream(file.path);
   if (_.endsWith(file.name, 'tar.gz')) {
-    // let dirName = _.get(file.name.split('.'), '0')
-    // let dirPath = path.resolve(__dirname, '../upload', dirName)
-    // if (dirName && fs.existsSync(dirPath) && fs.statSync(dirPath).isDirectory()) {
-    //   return ctx.body = '该gz文件已经上传，请检查';
-    // }
     targz.decompress({
       src: file.path,
-      dest: path.resolve(__dirname, '../', 'upload')
+      dest: path.resolve(__dirname, '../', 'hotpatch')
     }, function (err) {
       if (err) {
         console.log(err);
@@ -69,19 +64,22 @@ router.post('/', async (ctx, next) => {
   } else {
     const ext = file.name.split('.').pop();
     const originName = file.name.split('.').slice(0, file.name.split('.').length - 1).join('.')
-    if (originName.indexOf('/') > -1) {
-      await base.mkdirSync(path.resolve(__dirname, '../', 'upload', file.name))
+    if (originName === '') {
+      originName = file.name
     }
-    const upStream = fs.createWriteStream(`upload/${originName}.${ext}`);
+    if (originName.indexOf('/') > -1) {
+      await base.mkdirSync(path.resolve(__dirname, '../', 'hotpatch', file.name))
+    }
+    const upStream = fs.createWriteStream(`hotpatch/${originName}.${ext}`);
     reader.pipe(upStream);
   }
   return ctx.body = '上传成功';
 })
 
-router.get(/download\/(.*)$/, async (ctx, next) => {
+router.get(/hotpatch\/(.*)$/, async (ctx, next) => {
   const fileName = ctx.params[0];
   if (fileName) {
-    const path = `upload/${fileName}`
+    const path = `hotpatch/${fileName}`
     ctx.attachment(path);
     await send(ctx, path);
   } else {
@@ -102,56 +100,35 @@ router.post('/file/upload', upload.single('file'), async (ctx, next) => {
     await base.mkdirSync(chunksPath);
   }
   fs.renameSync(ctx.request.files.file.path, chunksPath + hash + '-' + index);
-  return ctx.body = '上传成功';
+  return ctx.body = '分片上传成功';
 })
 
 router.post('/file/merge_chunks', async (ctx, next) => {
   const {
     size, name, total, hash
   } = ctx.request.body;
-  // 根据hash值，获取分片文件。
-  // 创建存储文件
-  // 合并
   const chunksPath = path.join(uploadPath, hash, '/');
   const filePath = path.join(uploadPath, name);
-  // 读取所有的chunks 文件名存放在数组中
-  const chunks = fs.readdirSync(chunksPath);
-  // 创建存储文件
-  fs.writeFileSync(filePath, '');
-  if (chunks.length !== total || chunks.length === 0) {
+  let checkResult = base.fileChunkCheck(chunksPath, total)
+  if (checkResult) {
     ctx.status = 200;
     ctx.res.end('切片文件数量不符合');
-    return;
+  } else {
+    await base.fileMerge(chunksPath, filePath, total, hash)
   }
-  for (let i = 0; i < total; i++) {
-    // 追加写入到文件中
-    fs.appendFileSync(filePath, fs.readFileSync(chunksPath + hash + '-' + i));
-    // 删除本次使用的chunk
-    fs.unlinkSync(chunksPath + hash + '-' + i);
-  }
-  fs.rmdirSync(chunksPath);
-  // 文件合并成功，可以把文件信息进行入库。
+  // 针对gz文件处理，解压缩。
   if (_.endsWith(name, 'tar.gz')) {
-    targz.decompress({
-      src: filePath,
-      dest: path.resolve(__dirname, '../', 'upload')
-    }, function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        fs.unlinkSync(filePath)
-        console.log("Done!");
-      }
-    });
+    let destPath = path.resolve(__dirname, '../', 'hotpatch')
+    base.fileDecompress(filePath, destPath)
   }
   ctx.status = 200;
-  ctx.res.end('合并成功');
+  ctx.res.end('分片文件合并完成');
 })
 
 
 router.delete('/', async (ctx, next) => {
   const { fileName } = ctx.request.body;
-  const path = `upload/${fileName}`
+  const path = `hotpatch/${fileName}`
   fs.unlinkSync(path)
   return ctx.body = '删除成功';
 })
--- util/base.js
+++ util/base.js
@@ -1,12 +1,13 @@
 /*
  * @Auther: renjm
  * @Date: 2019-09-25 14:39:32
- * @LastEditTime: 2019-09-25 16:43:43
+ * @LastEditTime: 2019-10-22 18:33:43
  * @Description: 
  */
 
-const fs = require('fs')
+const fs = require('fs-extra');
 const _ = require('lodash')
+const targz = require('targz');
 module.exports.mkdirSync = async (dir) => {
     let index = 1;
     makeDir(dir, index);
@@ -28,4 +29,41 @@ makeDir = (dir, index) => {
         }
     }
     console.log('done')
+}
+
+
+module.exports.fileChunkCheck = (chunksPath, total) => {
+    const chunks = fs.readdirSync(chunksPath);
+    if (chunks.length !== total || chunks.length === 0) {
+        return true
+    }
+    return false
+}
+
+module.exports.fileMerge = async (chunksPath, filePath, total, hash) => {
+    // 创建存储文件
+    fs.writeFileSync(filePath, '');
+    // 读取所有的chunks 文件名存放在数组中
+    for (let i = 0; i < total; i++) {
+        // 追加写入到文件中
+        fs.appendFileSync(filePath, fs.readFileSync(chunksPath + hash + '-' + i));
+        // 删除本次使用的chunk
+        fs.unlinkSync(chunksPath + hash + '-' + i);
+    }
+    fs.rmdirSync(chunksPath);
+}
+
+// 解压缩
+module.exports.fileDecompress = async (filePath, destPath) => {
+    targz.decompress({
+        src: filePath,
+        dest: destPath
+    }, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.unlinkSync(filePath)
+            console.log("Done!");
+        }
+    });
 }
